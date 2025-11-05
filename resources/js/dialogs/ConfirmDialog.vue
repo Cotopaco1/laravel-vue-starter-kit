baa15497aed83341f3e28ef0cb5e4b4a6f4db855
Cotopaco1 <cotopaco123@hotmail.es>
2025-11-05 12:24:03 -0500

    feat: Add dialogs components

---
+++ resources/js/dialogs/ConfirmDialog.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+    import { Dialog, DialogContent, DialogFooter, DialogTitle, DialogDescription} from '@/components/ui/dialog';
+    import Button from '../ui/button/Button.vue';
+
+    const props = defineProps<{
+        callback : () => any;
+
+    }>();
+    const isOpen =  defineModel<boolean>('isOpen');
+    const handleConfirm = () => {
+        props.callback();
+        isOpen.value = false;
+    }
+</script>
+
+<template>
+    <Dialog v-model:open="isOpen">
+        <DialogContent>
+            <DialogTitle>¿Está seguro?</DialogTitle>
+            <DialogDescription>No es reversible esta acción</DialogDescription>
+            <DialogFooter>
+                <Button @click="handleConfirm">Confirmar</Button>
+            </DialogFooter>
+        </DialogContent>
+    </Dialog>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/dialogs/DialogLayout.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+    import { Dialog, DialogFooter, DialogTitle, DialogDescription } from '@/components/ui/dialog';
+    import DialogScrollContent from '@/components/ui/dialog/DialogScrollContent.vue';
+
+    const isOpen =  defineModel<boolean>('isOpen');
+
+</script>
+
+<template>
+    <Dialog v-model:open="isOpen">
+        <DialogScrollContent class="max-h-[90vh]">
+            <DialogTitle><slot name="title"></slot></DialogTitle>
+            <DialogDescription><slot name="description"></slot></DialogDescription>
+            <slot name="body"></slot>
+            <DialogFooter>
+                <slot name="footer"></slot>
+            </DialogFooter>
+        </DialogScrollContent>
+    </Dialog>
+</template>
+
+<style scoped>
+
+</style>
